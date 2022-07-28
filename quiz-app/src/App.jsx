21962a49a4c39185acefc10ce1dceb1f980f8baf
Aditya Raj Bhagat <adityabhagat900@gmail.com>
2022-07-29 02:40:39 +0530

    React-Quiz-App

---
+++ quiz-app/src/App.jsx
@@ -0,0 +1,97 @@
+import React, { useState } from 'react'
+
+export default function App() {
+
+	const [currentIndex, setCurrentIndex] = useState(0)
+    const [score, setScore] = useState(0)
+
+	const questions = [
+		{
+			questionText: 'What is the capital of France?',
+			answerOptions: [
+				{ answerText: 'New York', isCorrect: false },
+				{ answerText: 'London', isCorrect: false },
+				{ answerText: 'Paris', isCorrect: true },
+				{ answerText: 'Dublin', isCorrect: false },
+			],
+		},
+		{
+			questionText: 'Who is CEO of Tesla?',
+			answerOptions: [
+				{ answerText: 'Jeff Bezos', isCorrect: false },
+				{ answerText: 'Elon Musk', isCorrect: true },
+				{ answerText: 'Bill Gates', isCorrect: false },
+				{ answerText: 'Tony Stark', isCorrect: false },
+			],
+		},
+		{
+			questionText: 'The iPhone was created by which company?',
+			answerOptions: [
+				{ answerText: 'Apple', isCorrect: true },
+				{ answerText: 'Intel', isCorrect: false },
+				{ answerText: 'Amazon', isCorrect: false },
+				{ answerText: 'Microsoft', isCorrect: false },
+			],
+		},
+		{
+			questionText: 'How many Harry Potter books are there?',
+			answerOptions: [
+				{ answerText: '1', isCorrect: false },
+				{ answerText: '4', isCorrect: false },
+				{ answerText: '6', isCorrect: false },
+				{ answerText: '7', isCorrect: true },
+			],
+		},
+	]
+
+	function handleAnswerClick(isCorrect) {
+		
+        if (isCorrect) {
+            setScore((score) => ++score)
+        }
+		if (currentIndex === questions.length - 1) {
+			
+			setQuizFinished(true)
+		} else {
+			setCurrentIndex((value) => value + 1)
+		}
+	}
+
+	const [quizFinished, setQuizFinished] = useState(false)
+
+
+	return (
+		<div className="app">
+			{quizFinished ? (
+				
+				<div className="score-section">
+					You scored {score} out of {questions.length}
+				</div>
+			) : (
+				<>
+					<div className="question-section">
+						<div className="question-count">
+							<span>Question 1</span>/{questions.length}
+						</div>
+						<div className="question-text">
+							{questions[currentIndex].questionText}
+						</div>
+					</div>
+					<div className="answer-section">
+						{questions[currentIndex].answerOptions.map((answer) => {
+							
+                            return (
+								<button
+									onClick={() => handleAnswerClick(answer.isCorrect)}
+									key={answer.answerText}
+								>
+									{answer.answerText}
+								</button>
+							)
+						})}
+					</div>
+				</>
+			)}
+		</div>
+	)
+}
