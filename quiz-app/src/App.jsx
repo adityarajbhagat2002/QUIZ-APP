84092190cda5de68fc7962e6ed93aeadbee5a8ed
Aditya Raj Bhagat <adityabhagat900@gmail.com>
2022-07-29 13:02:23 +0530

    changes

--- quiz-app/src/App.jsx
+++ quiz-app/src/App.jsx
@@ -71,7 +71,7 @@ export default function App() {
 				<>
 					<div className="question-section">
 						<div className="question-count">
-							<span>Question 1</span>/{questions.length}
+							<span>Question {currentIndex+1}</span>/{questions.length}
 						</div>
 						<div className="question-text">
 							{questions[currentIndex].questionText}
